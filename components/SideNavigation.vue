4e2cf3979d5502d9ea94605a9a6a9f4621cecf6d
AlohaYos <yos@newtonjapan.com>
2020-04-05 15:38:08 +0900

    ライセンス加筆

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -91,7 +91,7 @@
           </a>
           {{ $t('の下に提供されています。') }}
           <br />
-          2020 Tokyo Metropolitan Government
+          2020 Tokyo Metropolitan Government, Yoshiyuki Hashimoto (@alohayos)
         </small>
       </footer>
     </div>
